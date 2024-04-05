6eaa48ccc341ff418c1e88e42a2cd7d358d0f57e
Prima Putra <primapwd@gmail.com>
2024-04-05 12:32:55 +0700

    feat: re-enable `Illuminate\Routing\Controller` as BaseController

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
-abstract class Controller
+abstract class Controller extends BaseController
 {
     use AuthorizesRequests;
 }
